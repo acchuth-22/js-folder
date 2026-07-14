130b893525748191d9d24546c820b646e61bca25
Acchuth Shekar <acchuths@gmail.com>
2026-07-14 07:13:33 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = 33
+//let score = "33"  //=> 33
+//let score ="33abc" //=> NAN
+//let score = null   //=> 0
+//let score = undefined  //=>undefined
+//let score = true  // => 1
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//let isLoggedIn = 1  //=> true
+//let isLoggedIn = ""  //=> false
+let isLoggedIn = "acchuth" //=>true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+let someNumber = 67
+let strinSomeNumber = String(someNumber)
+console.log(strinSomeNumber);
+console.log(typeof strinSomeNumber);
+
+
