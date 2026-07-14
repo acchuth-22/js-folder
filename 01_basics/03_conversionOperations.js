d5adfe6a7b5daa7f7fa72c8b8b5667ec7667d95b
Acchuth Shekar <acchuths@gmail.com>
2026-07-14 09:06:35 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,7 +23,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 67
 let strinSomeNumber = String(someNumber)
-console.log(strinSomeNumber);
-console.log(typeof strinSomeNumber);
+// console.log(strinSomeNumber);
+// console.log(typeof strinSomeNumber);
 
+//*************Operations***********//
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="acchu "
+let str2="hii"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
