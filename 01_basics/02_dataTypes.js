244b660b847a5c0c59ba053c11fd18835f98218a
Acchuth Shekar <acchuths@gmail.com>
2026-07-14 06:43:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  // use all JS code as newr version
+
+// alert(3+3) // we are using nodejs not browser
+
+let name = "acchuth"
+let age = 18
+let isLoggedin = true
+let State;
+
+//numeric=> 2 to power 53
+//bigint=> for larger interger
+//string=>""
+//boolean => true/false
+//null => standalone value (eg.,   let state = null  )
+//undefined
+//symbol => to find the uniqueness
+
+//object (null is an object)
+
+console.log(typeof "acchuth");
+console.log(typeof age);
+console.log(typeof null); //dataType is object
+console.log(typeof undefined);//dataType is undefined
+
+
+
