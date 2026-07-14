751456dc87c1e448ce9ec325b814f99355b6b667
Acchuth Shekar <acchuths@gmail.com>
2026-07-14 14:38:06 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// === for strictly check
+console.log("2"===2); //it doest check only the value it also check the dataTypes
+
