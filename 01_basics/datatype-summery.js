b4be33a16209de9cdee9fc072bf0437fe403f569
Acchuth Shekar <acchuths@gmail.com>
2026-07-14 15:25:15 +0000

    DataType Summery

---
+++ 01_basics/datatype-summery.js
@@ -0,0 +1,28 @@
+// Primitive dataType
+
+// 7 types: String, Number, Null, Boolean, Undefined, Symbol, BigInt
+
+const outSideTemp = null
+const bigNumber = 12346786543234678864n
+
+// Reference Type(Non Primitive dataTypes)
+
+// Array, Objects, Functions
+
+
+const heros = ['Spydy', 'Ironman', 'Loki']  //Array
+
+let myObj = {
+    name : "acchuth",    //the return inside the curly bracket is "Object"
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myFunction);
+
+
+
