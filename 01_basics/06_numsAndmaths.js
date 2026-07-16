18ba4f4523c9c91ace50780cd9df300e74048cd4
Acchuth Shekar <acchuths@gmail.com>
2026-07-16 09:03:10 +0000

    Numbers and Maths

---
+++ 01_basics/06_numsAndmaths.js
@@ -0,0 +1,40 @@
+const score =  400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const newNumber = 123.8526
+// console.log(newNumber.toPrecision(3));
+// console.log(newNumber.toPrecision(2));
+// console.log(newNumber.toPrecision(4));
+
+const Tens = 100000000
+// console.log(Tens.toLocaleString('en-IN'));
+
+
+//++++++++++++++  Maths  +++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));   //-4 will be come +ve and +ve will be +ve
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));     //take the high value
+// console.log(Math.floor(4.9));    //take the low value
+// console.log(Math.min(4,5.3,6,2,8));
+// console.log(Math.max(4,5.3,6,2,8));
+
+console.log(Math.random());   //random value will be btw 0 to 1 
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min)+1)+min)   // *****This line is VIMP, Remember it******
+
+
+
+
