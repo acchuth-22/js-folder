fac5833b19b13fe9416be4ff9e11c3eff80338a9
Acchuth Shekar <acchuths@gmail.com>
2026-07-23 16:36:38 +0000

    Three

---
+++ 05_iterations/three.js
@@ -0,0 +1,36 @@
+// for of loop
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    // console.log(`Eac element is ${greet}`);
+    
+}
+
+//Maps
+const map = new Map()   // map is a unique value
+map.set('IN','INDIA')
+map.set('USA','AMERICA')
+map.set('FR','FRANCE')
+// map.set('IN','INDIA')
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value );
+    
+}
+
+const myObject = {
+    game1 = "NFS",
+    game2 = "GTA"
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value );
+    
+}
