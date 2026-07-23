af8620d81acfa35edb0bca517de0518e39a5744d
Acchuth Shekar <acchuths@gmail.com>
2026-07-23 16:36:48 +0000

    Four

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+// for in loop
+
+const myObject = {
+    py : "Python",
+    cpp : "c++",
+    js : "Java Script",
+    rb : "Ruby"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shorcut is ${myObject[key]}`);
+}
+
+const programming = ['js','py', 'java', 'cpp', 'rb']
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
